491084b41f86d9b0f9f0bed9d9d7b776512fd7a8
Vince Skahan <vinceskahan@gmail.com>
2022-08-18 09:53:49 -0700

    bump ver

--- install.py
+++ install.py
@@ -17,7 +17,7 @@ def loader():
 class MemoryMonitorInstaller(ExtensionInstaller):
     def __init__(self):
         super(MemoryMonitorInstaller, self).__init__(
-            version="1.1",
+            version="1.2",
             name='mem',
             description='Collect and display process memory usage.',
             author="Vince Skahan",
